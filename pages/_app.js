ffd3a0bdecfc89a52e616bfdd8db9cff848446b1
Manoela Neves <manoela.nevesr@gmail.com>
2026-01-21 20:26:05 +0000

    modifica frase para enviar para victor

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "../pages/index.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 function Home() {
-    return <h1>Oi mãe te amo muitoooooo! estou aprendendo a colocar sites na internet! legal né?</h1>
+  const [mostrarCard, setMostrarCard] = useState(false);
+
+  return (
+    <div className="container">
+      <h1>Oi amo, clica no botão se vc me ama 🤍</h1>
+
+      <button onClick={() => setMostrarCard(true)}>
+        Clicar aqui
+      </button>
+
+      {mostrarCard && (
+        <div className="card">
+          <p>Eu te amo mais do que tudo 💖</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Home; 
+export default Home;
